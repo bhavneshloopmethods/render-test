363ad70592dfe7ef85518871e310e87aab84c2fd
bhavnesh <bhavneshsharma@loopmethods.com>
2024-11-10 17:54:40 +0530

    small changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get("/status",( req, res)=> {
+    res.status(200).json({ status: 'active'})
+})
 app.listen(2000, ()=> {
     console.log('server is listning on port 2000');
     
